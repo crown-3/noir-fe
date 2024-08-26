86e7ed09e313fbb1e8d2ea75e79874d96c0d4854
MiRoro2 <crowntheking@gm.gist.ac.kr>
2024-08-26 21:44:07 +0900

    feat: setup blank project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <></>;
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Layout from "./layout/Layout";
+import HomePage from "./pages/home/page";
+
+const App = () => {
+  const queryClient = new QueryClient();
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </>,
+    ),
+  );
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </>
+  );
+};
 
 export default App;
